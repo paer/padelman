ad962a901b8265245d2302c69d60d977cf10d257
Pär Linder <par.linder@stenametall.se>
2020-12-02 20:22:10 +0100

    Remove some generated files. Add icon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,14 +21,14 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#343434`,
+        theme_color: `#EFAD88`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/padelman-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const IndexPage = () => <h1>Padelman</h1>
+
+export default IndexPage
