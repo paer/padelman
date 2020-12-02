58b50b8686e035f4bcc07b0552c24db578197c6c
Pär Linder <par.linder@stenametall.se>
2020-12-02 23:35:13 +0100

    add clientcontext to function

--- functions/hello_world.js
+++ functions/hello_world.js
@@ -1,4 +1,7 @@
 exports.handler = function (event, context, callback) {
+  const { identity, user } = context.clientContext
+
+  console.log("user", user)
   callback(null, {
     statusCode: 200,
     body: "Hello, World",
